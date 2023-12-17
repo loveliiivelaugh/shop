dbf78ff8f6804d95ce846e497ab9ca98d8dc8ef8
M <woodward.michael.a@gmail.com>
2023-12-16 18:57:39 -0600

    Removed default React starter code. Added Mui Lib and dashboard components

---
+++ src/components/dashboard/Dashboard.js
@@ -0,0 +1,155 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom'
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import Badge from '@mui/material/Badge';
+import Container from '@mui/material/Container';
+import Link from '@mui/material/Link';
+import { Tooltip } from '@mui/material'
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import MenuIcon from '@mui/icons-material/Menu';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
+import HomeIcon from '@mui/icons-material/Home';
+import LightModeIcon from '@mui/icons-material/LightMode';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+
+// Hooks
+// import { useColorMode } from '../../../../contexts/ColorModeProvider';
+
+// Components
+import { mainListItems, secondaryListItems } from './listItems';
+
+// Styles
+import { AppBar, Drawer } from './styled.components';
+
+
+
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const NavigationBar = ({ open, setOpen }) => {
+  // const navigate = useNavigate()
+  // const { mode: colorMode, toggleMode } = useColorMode()
+  return (
+    <AppBar position="absolute" open={open}>
+      <Toolbar
+        sx={{
+          pr: '24px', // keep right padding when drawer closed
+        }}
+      >
+        <IconButton
+          edge="start"
+          color="inherit"
+          aria-label="open drawer"
+          onClick={() => setOpen(!open)}
+          sx={{
+            marginRight: '36px',
+            ...(open && { display: 'none' }),
+          }}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography
+          component="h1"
+          variant="h6"
+          color="inherit"
+          noWrap
+          sx={{ flexGrow: 1 }}
+        >
+          Dashboard
+        </Typography>
+        <IconButton color="inherit">
+          <Badge badgeContent={0} color="secondary">
+            <AccountBalanceWalletIcon />
+          </Badge>
+        </IconButton>
+        <IconButton color="inherit">
+          <Badge badgeContent={4} color="secondary">
+            <NotificationsIcon />
+          </Badge>
+        </IconButton>
+        {/* <IconButton color="inherit" onClick={() => navigate('/')}> */}
+        <IconButton color="inherit">
+          <HomeIcon />
+        </IconButton>
+        {/* <Tooltip title={colorMode === 'light' ? 'Light Mode' : 'Dark Mode'}>
+          <IconButton color="inherit" onClick={toggleMode}>
+            {colorMode === 'light' ? <LightModeIcon /> : <DarkModeIcon />}
+          </IconButton>
+        </Tooltip> */}
+      </Toolbar>
+    </AppBar>
+
+  )
+}
+
+const SidePanel = ({ open, setOpen }) => (
+  <Drawer variant="permanent" open={open}>
+    <Toolbar
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        px: [1],
+      }}
+    >
+      <IconButton onClick={() => setOpen(!open)}>
+        <ChevronLeftIcon />
+      </IconButton>
+    </Toolbar>
+    <Divider />
+    <List component="nav">
+      {mainListItems}
+      <Divider sx={{ my: 1 }} />
+      {secondaryListItems}
+    </List>
+  </Drawer>
+)
+
+
+function DashboardContent(props) {
+  const [open, setOpen] = useState(true);
+  return (
+      <Box sx={{ display: 'flex' }}>
+        <NavigationBar open={open} setOpen={setOpen} />
+        <SidePanel open={open} setOpen={setOpen} />
+        <Box
+          component="main"
+          sx={{
+            backgroundColor: (theme) =>
+              theme.palette.mode === 'light'
+                ? theme.palette.grey[100]
+                : theme.palette.grey[900],
+            flexGrow: 1,
+            height: '100vh',
+            overflow: 'auto',
+          }}
+        >
+          <Toolbar />
+          <Container maxWidth={false} sx={{ mt: 0, mb: 0 }}>
+            {props.children}
+            <Copyright sx={{ pt: 4 }} />
+          </Container>
+        </Box>
+      </Box>
+  );
+}
+
+const Dashboard = (props) => <DashboardContent {...props} />
+
+export default Dashboard
